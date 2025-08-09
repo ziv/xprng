f548c88a0ea2a3d01bf47d404089e6cf0f37624d
Ziv Perry <ziv.perry@gmail.com>
2025-08-09 11:45:21 +0300

    fix(vendor): initial commit

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 ng build @xprng/vendor
 ng build @xprng/common
 ng build @xprng/markdown
-ng build @xprng/code
+ng build @xprng/highlight
 
 cp license dist/xprng/vendor/.
 cp license dist/xprng/common/.
 cp license dist/xprng/markdown/.
-cp license dist/xprng/code/.
+cp license dist/xprng/highlight/.
---
+++ xprng/vendor/shiki/highlight-marked.ts
@@ -0,0 +1,24 @@
+import type {MarkedExtension} from "marked";
+import {getHighlighter} from "./get-highlighter";
+
+export function highlight(theme: string): MarkedExtension {
+  return {
+    async: false,
+    walkTokens(token) {
+      if (token.type !== "code") {
+        return;
+      }
+      const lang = (token.lang || "").match(/\S*/)?.[0] ?? "plaintext";
+      const text = token.text || "";
+
+      const html = getHighlighter().codeToHtml(text, {lang, theme});
+
+      // change the token type to 'html' and set the html content
+      Object.assign(token, {
+        type: "html",
+        block: true,
+        text: `${html}\n`,
+      });
+    },
+  };
+}
