bfe6ff6fc5eee160f6e952a83068432ab58a5eff
Ziv Perry <ziv.perry@gmail.com>
2025-08-26 14:14:16 +0300

    feat(vendor): add heading id extension to marked

--- xprng/vendor/marked/public-api.ts
+++ xprng/vendor/marked/public-api.ts
@@ -1,6 +1,7 @@
 import { Marked } from "marked";
 import { highlight } from "@xprng/vendor/shiki";
+import { gfmHeadingId } from "marked-gfm-heading-id";
 
 export function marked(theme: string) {
-  return new Marked(highlight(theme));
+  return new Marked(highlight(theme), gfmHeadingId());
 }
