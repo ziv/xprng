ba98be9f52177ee569908abd248cfa4b4dbd9519
Ziv Perry <ziv.perry@gmail.com>
2025-08-27 12:40:49 +0300

    feat(vendor): add twoandhalfslash support

--- xprng/vendor/shiki/highlight-marked.ts
+++ xprng/vendor/shiki/highlight-marked.ts
@@ -1,5 +1,6 @@
 import type {MarkedExtension} from "marked";
 import {getHighlighter} from "./get-highlighter";
+import {thsTransformer, twoandhalfslash} from 'twoandhalfslash';
 
 /**
  * Highlight content blocks in marked using Shiki.
@@ -15,10 +16,11 @@ export function highlight(theme: string): MarkedExtension {
       const lang = (token.lang ?? "plaintext").toLowerCase().trim();
       const text = token.text ?? "";
 
-      const html = getHighlighter().codeToHtml(text, {
+      const html = twoandhalfslash(getHighlighter().codeToHtml(text, {
         lang,
         theme,
-      });
+        transformers: [thsTransformer]
+      }));
 
       // change the token type to 'html' and set the html code
       Object.assign(token, {
