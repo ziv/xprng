856f531348446a05107fa198cc7be190b2cbce31
Ziv Perry <ziv.perry@zoominfo.com>
2025-09-28 10:53:00 +0300

    remove xpd

--- apps/xpd/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import Config from "./app/app.config";
-import { XpdShell } from "@xprng/docs";
-
-bootstrapApplication(XpdShell, Config).catch((err) => console.error(err));
