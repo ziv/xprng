84f06fb012fd18b6e5ff5610e859523a3a3d2e2c
Ziv Perry <ziv.perry@zoominfo.com>
2025-12-24 17:23:23 +0200

    test: improve test runner

--- ng-build.sh
+++ ng-build.sh
@@ -1,15 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-ng build @xprng/vendor
-ng build @xprng/common
-ng build @xprng/markdown
-ng build @xprng/code
-ng build @xprng/slides
-ng build @xprng/query
+projects=(vendor common markdown code slides query)
 
-cp license dist/xprng/vendor/.
-cp license dist/xprng/common/.
-cp license dist/xprng/markdown/.
-cp license dist/xprng/code/.
-cp license dist/xprng/slides/.
-cp license dist/xprng/query/.
+for project in "${projects[@]}"; do
+  ng build "@xprng/$project"
+done
+
+for project in "${projects[@]}"; do
+  cp license "dist/xprng/$project/"
+done
--- ng-test.sh
+++ ng-test.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-#ng test --watch false @xprng/vendor
-ng test --watch false @xprng/common
-ng test --watch false @xprng/markdown
-ng test --watch false @xprng/code
-ng test --watch false @xprng/slides
+projects=(common markdown code slides)
+
+for project in "${projects[@]}"; do
+  ng test --watch false "@xprng/$project"
+done
